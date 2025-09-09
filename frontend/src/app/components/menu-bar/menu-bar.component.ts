8c99fb6543512060ab176939badf0716073e5e5e
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-09-09 18:22:35 +0200

    Add router

--- frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterLink,
+} from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css',
 })
 export class MenuBarComponent {
-  currentPage = signal<string>('');
+  currentRoute = signal<string>('');
 
-  handleClick(e: any) {
-    this.currentPage.set(e.target.closest('li').id);
+  constructor(private router: Router) {
+    // Hämta och spara nuvarande route
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute.set(event.url);
+      });
   }
 }
