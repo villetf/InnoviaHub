c3730f77314ad4b10c085f6ae122041c3e51346b
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-09-09 18:21:43 +0200

    Add page components

---
+++ frontend/src/app/pages/sensor-page/sensor-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-page',
+  imports: [],
+  templateUrl: './sensor-page.component.html',
+  styleUrl: './sensor-page.component.css'
+})
+export class SensorPageComponent {
+
+}
