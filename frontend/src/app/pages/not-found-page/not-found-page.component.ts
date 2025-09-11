227535f9a9d3b5c8499e0cf610c2785f89775828
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-09-11 15:00:07 +0200

    Develop not found page

--- frontend/src/app/pages/not-found-page/not-found-page.component.ts
+++ frontend/src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found-page.component.html',
-  styleUrl: './not-found-page.component.css'
+  styleUrl: './not-found-page.component.css',
 })
-export class NotFoundPageComponent {
-
-}
+export class NotFoundPageComponent {}
