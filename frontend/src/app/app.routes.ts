f83ef5295bd3fae417a20340d64b4297e951702f
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-09-11 09:33:50 +0200

    Frontend - Inloggningssida/landningssida #59 utan backend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.com
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { BookingPageComponent } from './pages/booking-page/booking-page.component';
 import { SensorPageComponent } from './pages/sensor-page/sensor-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
 export const routes: Routes = [
+  { path: 'logga-in', component: LoginPageComponent },
+  { path: 'registrera', component: RegistrationPageComponent },
   { path: 'profil', component: ProfilePageComponent },
   { path: 'boka', component: BookingPageComponent },
   { path: 'sensorer', component: SensorPageComponent },
-  //   { path: '', redirectTo: 'inloggning?', pathMatch: 'full' },
+  { path: '', redirectTo: 'logga-in', pathMatch: 'full' },
   { path: '**', component: NotFoundPageComponent },
 ];
