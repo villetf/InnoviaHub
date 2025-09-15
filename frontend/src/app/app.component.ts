d67e4ec7d06d348a1a18d6299f2625df72724126
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-09-15 13:17:54 +0200

    Remove test component from app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { filter } from 'rxjs';
-import { BookingConfirmationPopupComponent } from './components/booking-confirmation-popup/booking-confirmation-popup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuBarComponent, BookingConfirmationPopupComponent],
+  imports: [RouterOutlet, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
